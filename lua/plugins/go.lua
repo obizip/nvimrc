1da488efd20877a216166beb38af549a10674420
obi-3 <ct4kuto@gmail.com>
2023-01-30 22:03:17 +0900

    vim-go: disable default keymaps

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -2,5 +2,8 @@ return {
   "fatih/vim-go",
   ft = { "go" },
   event = "VeryLazy",
-  config = function() end,
+  config = function()
+    vim.cmd("let g:go_doc_keywordprg_enabled = 0")
+    vim.cmd("let g:go_def_mapping_enabled = 0")
+  end,
 }
