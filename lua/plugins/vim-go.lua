6b45ea4eb77906e763b10498573d616533f7906c
obi-3 <ct4kuto@gmail.com>
2023-01-31 22:52:04 +0900

    Rename go.lua -> vim-go.lua

--- lua/plugins/vim-go.lua
+++ lua/plugins/vim-go.lua
@@ -5,5 +5,6 @@ return {
   config = function()
     vim.cmd("let g:go_doc_keywordprg_enabled = 0")
     vim.cmd("let g:go_def_mapping_enabled = 0")
+    vim.cmd("let g:go_metalinter_autosave = 1")
   end,
 }
