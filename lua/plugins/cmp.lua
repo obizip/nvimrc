0f573d1002117b56af76fddd4f50f31c6f793a7c
obi-3 <ct4kuto@gmail.com>
2023-02-01 08:04:20 +0900

    Add a lspkind plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,12 +14,13 @@ local M = {
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "dmitmel/cmp-cmdline-history",
     'ray-x/cmp-treesitter',
-    -- 'nvim-orgmode/orgmode'
+    'onsails/lspkind.nvim',
   },
 }
 
 function M.config()
 local cmp = require("cmp")
+local lspkind = require('lspkind')
 
 cmp.setup({
   snippet = {
@@ -55,6 +56,19 @@ cmp.setup({
         hl_group = "LspCodeLens",
       },
     },
+    formatting = {
+      format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+    }
   })
 
 -- Use buffer source for `/` and `?`
