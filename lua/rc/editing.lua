645be1921c2ba0bb5b71e577849fa73517a2bcc1
obizip <ct4kuto@gmail.com>
2023-12-27 08:51:38 +0900

    Delete peek.nvim because of server situation

--- lua/rc/editing.lua
+++ lua/rc/editing.lua
@@ -43,17 +43,17 @@ return {
             },
         },
     },
-    {
-        "toppair/peek.nvim",
-        event = { "VeryLazy" },
-        build = "deno task --quiet build:fast",
-        config = function()
-            require("peek").setup()
-            -- refer to `configuration to change defaults`
-            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-        end,
-    },
+    -- {
+    --     "toppair/peek.nvim",
+    --     event = { "VeryLazy" },
+    --     build = "deno task --quiet build:fast",
+    --     config = function()
+    --         require("peek").setup()
+    --         -- refer to `configuration to change defaults`
+    --         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    --         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    --     end,
+    -- },
     -- {
     --     "folke/flash.nvim",
     --     event = "VeryLazy",
